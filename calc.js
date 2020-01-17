846f0bbd31844a32db34d8c8d49cc1503dbaab43
EDA <james.panaho@gmail.com>
2020-01-17 13:41:15 +1300

    functions made

--- calc.js
+++ calc.js
@@ -1,42 +1,61 @@
 // variables
-let tempArray
-let prevButton
-let prevArray
-let storedTotal
+let tempArray = []
+let prevString = ''
+let storedTotal = ''
 // display
 let display = document.getElementById('display')
-console.log(document)
-
-// start function
-start()
 
 // what button did you enter
 function start () {
-  document.addEventListener('click', getButtonValue)
+  document.addEventListener('click', addToTemp)
 }
 // if button is a number add to current string
-function getButtonValue () {
-  console.log(event)
+function addToTemp () {
   let button = event.target.value
-
-  console.log(button)
-  if (button === '.') {
-    checkforDot()
+  if (!isNaN(button) && button !== '0') {
+    tempArray.push(button)
+  } else if (button === 'AC') {
+    allClear()
+  } else if (button === 'CE') {
+    clear()
+  } else if (button === '.') {
+    checkforDot(button)
   } else if (button === '0') {
-    checkforZero()
+    checkforZero(button)
+  } else if (button === '+' || button === '-' || button === '*' || button === '/') {
+    calculate(button)
+  } else if (button === '='){
+    total()
   }
-
 }
 // store current Array, display values
-//
+
+//clear display functions
+allClear() {
+
+}
+
+clear() {
+
+}
 
 // // other functions
-// // if we click 0 twice
-// checkforZero() {
+checkforDot() {
 
-// }
-// // if we click . twice
-// checkforDot(){
+}
 
-// }
-// // if we
+checkforZero(){
+
+}
+
+calculate(button){
+const stringNum = tempArray.join('') + button
+prevString = stringNum
+}
+
+total () {
+
+}
+
+// start function
+start()
